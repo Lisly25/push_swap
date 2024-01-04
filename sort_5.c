9b49e7d9a047c4fa5e02dacd7d54a9ba63ef4a55
Szabina Korbai <skorbai@c1r5p7.hive.fi>
2024-01-04 11:21:53 +0200

    Lists of 4 numbers are now sorted properly

--- sort_5.c
+++ sort_5.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:49:26 by skorbai           #+#    #+#             */
-/*   Updated: 2024/01/04 09:55:19 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/01/04 11:19:23 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,45 @@ static char	*sort_last_3(int **a, int i)
 	return ("");
 }
 
+static size_t	count_of_greater_than(int **a, int num, size_t count)
+{
+	size_t	i;
+	size_t	greater_than;
+
+	i = 0;
+	greater_than = 0;
+	while (i < count)
+	{
+		if (a[i][0] < num)
+			greater_than++;
+		i++;
+	}
+	return (greater_than);
+}
+
+
 char	*sort_all(int **a, size_t count)
 {
 	char	*result;
-	char	*temp;
+	size_t	greater_than;
 
 	if (count == 4)
 	{
-		if (a[0][0] > a[1][0] && a[0][0] > a[2][0] && a[0][0] > a[3][0])
+		greater_than = count_of_greater_than(a, a[0][0], count);
+		if (greater_than == 3)
 			result = ft_strjoin("pa\n", "ra\n");
+		if (greater_than == 0)
+			result = ft_strdup("pa\n");
+		if (greater_than == 1)
+			result = ft_strjoin("pa\n", "sa\n");
+		if (greater_than == 2)
+			result = ft_strjoin("rra\npa\n", "ra\nra\n");
 	}
 	if (count == 5)
 	{
-		
+		result = ft_strdup("Not yet coded");
 	}
+	return (result);
 }
 
 char	*sort_5(int **a, size_t count)
@@ -71,5 +96,6 @@ char	*sort_5(int **a, size_t count)
 	sort_all_result = sort_all(a, count);
 	result = ft_strjoin(temp, sort_all_result);
 	free(temp);
+	free(sort_all_result);
 	return (result);
 }
