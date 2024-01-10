93abc8cefd751d958da21c4f349a34934ae18be3
Szabina Korbai <skorbai@c1r2p1.hive.fi>
2024-01-10 10:45:59 +0200

    Stack 'a' is now properly freed even in case of error

--- check_for_errors.c
+++ check_for_errors.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 11:24:40 by skorbai           #+#    #+#             */
-/*   Updated: 2024/01/05 11:53:38 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/01/10 10:45:03 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,9 +84,13 @@ int	check_for_errors(int **a)
 	if (check_for_duplicates(a, stack_size) == 1)
 	{
 		ft_putendl_fd("Error", 2);
+		free_stack(a);
 		return (1);
 	}
 	if (check_if_already_sorted(a, stack_size) == 1)
+	{
+		free_stack(a);
 		return (1);
+	}
 	return (0);
 }
