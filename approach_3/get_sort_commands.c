f266797d4acd5c43ec1731e31109ea149f225cfd
Szabina Korbai <skorbai@c3r5p1.hive.fi>
2024-01-23 11:13:26 +0200

    Removed (now) unused variable

--- approach_3/get_sort_commands.c
+++ approach_3/get_sort_commands.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 13:36:10 by skorbai           #+#    #+#             */
-/*   Updated: 2024/01/23 10:23:33 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/01/23 11:11:56 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static char	*sort_3(int **a, size_t count)
 	return ("");
 }
 
-static void	sort_4(t_stacks *stacks, size_t size)
+static void	sort_4(t_stacks *stacks)
 {
 	stacks = ft_push_to_b(stacks);
 	ft_printf("pb\n");
@@ -61,7 +61,7 @@ void	get_sort_commands(t_stacks *stacks)
 	}
 	else if (i == 4)
 	{
-		sort_4(stacks, i);
+		sort_4(stacks);
 	}
 	else
 		sort_large(stacks, i);
